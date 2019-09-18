651f5baedaf3b83b6c08e0d1a607dea34061a7c5
Student <student@Makerss-MacBook-Pro.local>
2019-09-18 16:06:04 +0100

    Using Instance Variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,17 @@
 require_relative "bike"
 
 class DockingStation
+
+attr_reader :bike
+
+  def dock(bike)
+    @bike = bike
+  end
+
   def release_bike
     Bike.new
   end
+
 end
 
 #station = DockingStation.new
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,9 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
 end
