9b806c45b33dd0283eee522dea2462d9513be428
Student <student@Makerss-MacBook-Pro.local>
2019-09-19 16:10:47 +0100

    added report broken feature

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,13 @@
 require "bike"
 describe Bike do
   it { is_expected.to respond_to :working? }
+
+  describe '#report_broken' do
+
+    it 'user reports bike broken' do
+      bike = Bike.new
+      bike.report_broken
+      expect(bike.working?).to eq false 
+    end
+  end
 end
